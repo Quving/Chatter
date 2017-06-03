1afc074726a148160913a44536b4b6586d125bee
Vinh Ngu <vinh-ngu@hotmail.com>
2017-06-03 21:52:38 +0200

    Chatter client takes arguments.

--- src/main/java/Orange/Chatter/Client/ClientMain.java
+++ src/main/java/Orange/Chatter/Client/ClientMain.java
@@ -3,7 +3,13 @@ package Orange.Chatter.Client;
 public class ClientMain {
 
 	public static void main(String[] args) {
-		Client client = new Client("vingu.online", 25552);
+		Client client;
+		if (args.length == 2) {
+			client = new Client(args[0], Integer.parseInt(args[1]));
+
+		} else {
+			client = new Client("vingu.online", 25552);
+		}
 		client.connect();
 	}
 }
